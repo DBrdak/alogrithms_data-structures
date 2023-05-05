6ebde0b85726547b7ea1d6d359bbff2b8c60d65b
DBrdak <brdak02dominik@gmail.com>
2023-05-05 15:11:07 +0200

    Add project files.

---
+++ Array/Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Blind75/Array/Two Sum/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Two_Sum
+{
+    internal class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if(i == j)
+                        continue;
+
+                    if (nums[i] + nums[j] == target)
+                        return new[] { i, j };
+                }
+            }
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            var input = TwoSum(new[] { 3, 2, 4, 3, 4, 1, 5 }, 9);
+
+            if(input is not null)
+                Console.WriteLine($"[{input[0]},{input[1]}]");
+        }
+    }
+}
