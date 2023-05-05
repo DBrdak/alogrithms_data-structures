8b654089c7ae528056ae829ab9f3d7e7eeb56824
DBrdak <brdak02dominik@gmail.com>
2023-05-05 16:07:54 +0200

    Stock & Duplicate

---
+++ Array/Best Time to Buy and Sell Stock/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    internal class Solution
+    {
+        public static int MaxProfit(int[] prices)
+        {
+        //    var profit = 0;
+
+        //    for (int i = 0; i < prices.Length; i++)
+        //    {
+        //        for (int j = i+1; j < prices.Length; j++)
+        //        {
+        //            if (prices[j] - prices[i] > profit)
+        //                profit = prices[j] - prices[i];
+        //        }
+        //    }
+        //    return profit;
+
+            var maxProfit = new List<int>();
+            var buyPrice = prices[0];
+            var sellPrice = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] > sellPrice)
+                    sellPrice = prices[i];
+
+                else if (i < prices.Length - 1 && 
+                        prices[i] < buyPrice)
+                {
+                    buyPrice = prices[i];
+                    sellPrice = prices[i];
+                }
+
+                maxProfit.Add(sellPrice - buyPrice);
+            }
+
+            return maxProfit.Max();
+        }
+
+        static void Main(string[] args)
+        {
+            var result = MaxProfit(new[] { 7, 6, 4, 3, 1 });
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Array/Contains Duplicate/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace Contains_Duplicate
+{
+    internal class Solution
+    {
+        public static bool ContainsDuplicate(int[] nums) => !nums.SequenceEqual(nums.Distinct());
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ContainsDuplicate(new []{1,2,3,3 }));
+        }
+    }
+}
