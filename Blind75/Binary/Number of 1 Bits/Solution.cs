f2c0c8094426a10e63bd35cba07b3c9dcd496b95
DBrdak <brdak02dominik@gmail.com>
2023-05-08 11:12:48 +0200

    Number of 1 bits

---
+++ Blind75/Binary/Number of 1 Bits/Solution.cs
@@ -0,0 +1,14 @@
+﻿using static System.Int32;
+
+namespace Number_of_1_Bits
+{
+    internal class Program
+    {
+        public static int HammingWeight(uint n) => PopCount((int)n);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HammingWeight(00000000000000000000000000001011));
+        }
+    }
+}
---
+++ Blind75/Binary/Sum of Two Integers/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Sum_of_Two_Integers
+{
+    internal class Solution
+    {
+        public static int GetSum(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = a;
+                a = a ^ b;
+                b = (b & temp) << 1;
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSum(2,4));
+        }
+    }
+}
